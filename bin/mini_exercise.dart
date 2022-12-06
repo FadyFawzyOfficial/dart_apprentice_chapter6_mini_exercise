5d9b7dfcadf0961ab73cb9284a74a84b47a8c6fe
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2022-12-06 11:05:49 +0200

    6.1.1. Dart classes: Mini-exercise 1

--- bin/mini_exercise.dart
+++ bin/mini_exercise.dart
@@ -1,3 +1,12 @@
-void main() {
-  print('Hello world!');
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+void main() {}
+
+/// Dart classes
+///
+/// Dart classes: Mini-exercise 1
+///
+/// Create a class called `Password` and give it a string property
+/// called `value`.
+class Password {
+  String value = '';
 }
