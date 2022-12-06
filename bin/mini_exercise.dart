394263617ab2f052b8d9f5a5b66aa11328d10538
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2022-12-06 11:10:50 +0200

    6.1.3. Dart classes: Mini-exercise 3

--- bin/mini_exercise.dart
+++ bin/mini_exercise.dart
@@ -15,8 +15,23 @@ void main() {}
 /// Dart classes: Mini-exercise 2
 ///
 /// Override the `toString` method of `Password` so that it prints `value`.
+
+// class Password {
+//   String value = '';
+
+//   @override
+//   String toString() => 'Password(value: $value)';
+// }
+
+/// Dart classes: Mini-exercise 3
+///
+/// Add a method to `Password` called `isValid` that returns `true` only
+/// if the length of `value` is greater than `8`.
+
 class Password {
   String value = '';
+
+  bool get isValid => value.length > 8;
 
   @override
   String toString() => 'Password(value: $value)';
